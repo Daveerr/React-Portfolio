56c7fc7be551ac2abae9d04ec76e08c7529bf139
David Rodriguez <Daviderrr10@gmail.com>
2023-06-20 23:26:51 -0400

    nav bar

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -3,16 +3,24 @@ import Logo from "../assets/DR.png";
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#182825]">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#182825] text-white">
       <div>
-        <img src={Logo} alt="" style={{ width: "60px" }} />
+        <img src={Logo} alt="" style={{ width: "50px" }} />
       </div>
+      {/* nav bar menu */}
       <div>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul className="flex">
+          <li>About Me</li>
+          <li>Portfolio</li>
+          <li>Contact Me</li>
+          <li>Skills</li>
+        </ul>
+        {/* mobile nav */}
+        <ul className="hidden">
+          <li>About Me</li>
+          <li>Portfolio</li>
+          <li>Contact Me</li>
+          <li>Skills</li>
         </ul>
       </div>
     </div>
